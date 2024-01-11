b57e65908852b97e6bf82fe08ffa6e92cab5255d
AxelCourtois <courtois.a@hotmail.com>
2024-01-11 13:27:00 +0100

    oué oué

---
+++ character.js
@@ -0,0 +1,155 @@
+export { Character, Fighter, Paladin, Monk, Berzerker, Assassin };
+
+class Character {
+  constructor(name, hp, dmg, mana) {
+    this.name = name;
+    this.hp = hp;
+    this.dmg = dmg;
+    this.mana = mana;
+    this.status = "alive";
+  }
+
+  takeDamage(damage) {
+    if (this.reduceDamage) {
+      damage -= this.reduceDamage;
+    }
+    this.hp -= damage;
+    if (this.hp <= 0) {
+      this.hp = 0;
+      this.status = "dead";
+    }
+  }
+
+  dealDamage(target) {
+    if (target.status !== "dead") {
+      target.takeDamage(this.dmg);
+      if (target.status === "dead") {
+        this.mana += 20;
+      }
+    }
+  } 
+
+  resetReduceDamage() {
+    this.reduceDamage = null
+  }
+}
+
+
+
+class Fighter extends Character {
+  constructor(name) {
+    super(name, 12, 4, 40);
+    this.spellCost = 20;
+  }
+
+  darkVision(target) {
+    if (this.mana >= this.spellCost) {
+      console.log(`${this.name} utilise son attaque spéciale Dark Vision sur ${target.name} !`)
+      this.mana -= this.spellCost;
+      target.takeDamage(5);
+      console.log(`${target.name} perd 5 pv et réduis les dégats de 2 pour le tour !`)
+      this.reduceDamage = 2;
+    }
+    else {
+      console.log(`${this.name} croit pouvoir faire son attaque spéciale mais QUE NENI, il n'a pas assez de mana...`)
+    }
+  }
+}
+
+class Paladin extends Character {
+  constructor(name) {
+    super(name, 16, 3, 160);
+    this.spellCost = 40;
+  }
+
+  healingLighting(target) {
+    if (this.mana >= this.spellCost) {
+      console.log(`${this.name} utilise son attaque spéciale Healing Lighting !`)
+      this.mana -= this.spellCost;
+      target.takeDamage(4);
+      if (this.hp = 16) {
+        console.log(`${target.name} perd 4 pv`)
+        target.hp -= 4;
+      }
+      else if (this.hp >= 11) {
+        console.log(`${target.name} perd 4 pv et ${this.name} se soigne entièrement récupère ${16 - this.hp} pv.`)
+        this.hp = 16;
+        target.hp -= 4;
+      }
+      else {
+        console.log(`${target.name} perd 4 pv et ${this.name} se soigne de 5 pv.`)
+        this.hp += 5;
+        target.hp -= 4;
+      }
+    }
+    else {
+      console.log(`${this.name} croit pouvoir faire son attaque spéciale mais QUE NENI, il n'a pas assez de mana...`)
+    }
+  }
+}
+
+class Monk extends Character {
+  constructor(name) {
+    super(name, 8, 2, 200);
+    this.spellCost = 25;
+  }
+
+  heal() {
+    if (this.mana >= this.spellCost) {
+      console.log(`${this.name} utilise son attaque spéciale Heal !`)
+      this.mana -= this.spellCost;
+      if (this.hp = 8) {
+        console.log(`Il ne se passe rien... ${this.name} est déjà au max de ses pv.`)
+      }
+      else if (this.hp >= 1) {
+        this.hp = 8;
+        console.log(`${this.name} régénère sa vie entièrement en récupérant ${8 - this.hp} pv.`)
+      }
+    }
+    else {
+      console.log(`${this.name} croit pouvoir faire son attaque spéciale mais QUE NENI, il n'a pas assez de mana...`)
+    }
+  }
+}
+
+class Berzerker extends Character {
+  constructor(name) {
+    super(name, 12, 4, 0);
+    this.spellCost = 0;
+  }
+
+  rage() {
+    console.log(`${this.name} utilise son attaque spéciale Rage !`)
+    this.dmg += 1;
+    this.hp -= 1;
+    console.log(`${this.name} perd 1 pv et inflige désormais 1 dégat supplémentaire!`)
+  }
+}
+
+class Assassin extends Character {
+  constructor(name) {
+    super(name, 6, 6, 20);
+    this.spellCost = 20;
+  }
+
+  shadowHit(target) {
+    if (this.mana >= this.spellCost) {
+      console.log(`${this.name} utilise son attaque spéciale Shadow Hit !`)
+      this.mana -= this.spellCost;
+      if (target.hp <= 7) {
+        console.log(`${target.name} perd 7 pv et meurt. ${this.name} est insensible pour le tour.`)
+        this.reduceDamage = 100;
+        target.status = "dead";
+      }
+      else {
+        console.log(`${this.name} se suicide, dommage bouffon!`)
+        this.hp = 0
+        this.status = "dead"
+      }
+    }
+    else {
+      console.log(`${this.name} croit pouvoir faire son attaque spéciale mais QUE NENI, il n'a pas assez de mana...`)
+    }
+  }
+}
+
---
+++ game.js
@@ -0,0 +1,145 @@
+
+import { Character, Fighter, Paladin, Monk, Berzerker, Assassin } from './character.js';
+
+class Game {
+  constructor(players) {
+    this.players = players;
+    this.turnLeft = 10;
+  }
+
+
+  startGame() {
+    console.log("Le jeu commence !");
+    while (this.turnLeft > 0 && this.players.filter(player => player.status !== "dead").length > 1) {
+      this.startTurn();
+      this.players = this.shuffle(this.players);
+      this.playTurn();
+      this.skipTurn();
+      this.checkEndGame();
+    }
+  }
+  
+  playTurn() {
+    this.players.forEach(player => {
+      if (player.status !== "dead") {
+        console.log(`-> C'est au tour de ${player.name} de jouer.`);
+        let randomPlayer = this.getRandomPlayer(player);
+        if (player.name === "Axel le beau") {
+          let action;
+          do {
+            action = prompt("Tu préfères attaquer ou utiliser ton attaque spéciale ? Tu peux consulter les stats avec w (a/s/w)");
+            if (action === "a") {
+              this.performAttack(player, randomPlayer);
+            } else if (action === "s") {
+              this.performSpecialAttack(player, randomPlayer);
+            } else if (action === "w") {
+              this.watchStats();
+            }
+          } while (action === "w"); // si l'utilisateur choisit "w", demander à nouveau une action
+        } else {
+          this.performAttack(player, randomPlayer);
+        }
+      }
+    });
+  }
+  
+  getRandomPlayer(currentPlayer) {
+    let playersAlive = this.players.filter(p => p.status !== "dead" && p !== currentPlayer);
+    let randomPlayer;
+    do {
+      randomPlayer = playersAlive[Math.floor(Math.random() * playersAlive.length)];
+    } while (randomPlayer.status === "dead");
+    return randomPlayer;
+  }
+  
+  performAttack(player, target) {
+    let randomAttack = Math.random();
+    if (randomAttack < 0.5 || player.mana < player.spellCost) {
+      player.dealDamage(target);
+      console.log(`${player.name} fait une attaque normale sur ${target.name} et lui inflige ${player.dmg} points de dégâts !`);
+    } else {
+      this.performSpecialAttack(player, target);
+    }
+  }
+  
+  performSpecialAttack(player, target) {
+    switch (player.constructor.name) {
+      case "Fighter":
+        player.darkVision(target);
+        break;
+      case "Paladin":
+        player.healingLighting(target);
+        break;
+      case "Monk":
+        player.heal();
+        break;
+      case "Berzerker":
+        player.rage();
+        break;
+      case "Assassin":
+        player.shadowHit(target);
+        break;
+    }
+  }
+  
+
+  // Tirage aléatoire des joueurs 
+  shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    // Tant qu'il reste des éléments à mélanger
+    while (0 !== currentIndex) {
+      // Prendre un élément restant
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // Et échanger avec l'élément actuel
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    // Renvoyer le tableau mélangé
+    return array;
+  }
+
+  startTurn() {
+    console.log("------ > ROUND " + (10 - this.turnLeft + 1) + " < ------");
+  }
+
+  skipTurn() { 
+    console.log("------------------------ FIN DU ROUND ------------------------------");
+    this.players.forEach(player => {
+      player.resetReduceDamage();
+    });
+    this.turnLeft -= 1;
+  }
+
+  watchStats() {
+    console.log(axel);
+    console.log(lalaina);
+    console.log(alex);
+    console.log(sam);
+    console.log(louis);
+  }
+
+  checkEndGame() {
+    let alivePlayers = this.players.filter(player => player.status === "alive");
+
+    if (this.turnLeft === 0) {
+      let winner = alivePlayers.reduce((prev, current) => (prev.hp > current.hp) ? prev : current);
+      console.log(`Le gagnant est ${winner.name} avec ${winner.hp} points de vie restants !`);
+    } else if (alivePlayers.length === 1) {
+      console.log(`Le gagnant est ${alivePlayers[0].name} !`);
+    }
+  }
+}
+
+
+let axel = new Fighter("Axel le beau");
+let lalaina = new Paladin("Lalaina la belle");
+let alex = new Monk("Alex la chipo");
+let sam = new Berzerker("Sam la merguez");
+let louis = new Assassin("Louis la chips");
+
+let game = new Game([axel, lalaina, alex, louis, sam]);
+
+game.startGame();
+
